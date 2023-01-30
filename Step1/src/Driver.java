9c872cbfec7cc401896b165c0ad4cbc532e6dd3d
PkonradW <pkonrad2014@gmail.com>
2023-01-30 17:50:18 -0500

    Figuring out how to retrieve values

--- Step1/src/Driver.java
+++ Step1/src/Driver.java
@@ -6,11 +6,18 @@ import java.io.IOException;
 public class Driver {
     public static void main(String[] args) throws IOException {
         // create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        @SuppressWarnings("deprecation") ANTLRInputStream input = new ANTLRInputStream(System.in);
         // create a lexer that feeds off of input CharStream
         LittleLexer lexer = new LittleLexer(input);
         // create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
-
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // LittleParser parser = new LittleParser(tokens);
+        // ParseTree tree = parser.prog(); // begin parsing at prog rule
+        // System.out.println(tree.toStringTree(parser));
+        System.out.println(tokens.getNumberOfOnChannelTokens());
+        for (int i = 0; i < tokens.getNumberOfOnChannelTokens(); i++){
+            // correlates to the values in Little.tokens
+            System.out.print(i + ":" + tokens.get(i).getType() + " ");
+        }
     }
 }
